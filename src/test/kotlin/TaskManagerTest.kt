f86c45102a7d552f19792a7cdc0a6d7ddc6d3b18
IB <baitcode@baitbook.local>
2023-12-01 16:26:51 +0000

    correct check if error is subclass + correct test for json deserialisation errors

--- src/test/kotlin/TaskManagerTest.kt
+++ src/test/kotlin/TaskManagerTest.kt
@@ -12,12 +12,13 @@ import io.kotest.framework.concurrency.continually
 import io.kotest.framework.concurrency.eventually
 import io.kotest.framework.concurrency.until
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
+import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.launch
-import kotlinx.datetime.Clock
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
 import org.testcontainers.containers.wait.strategy.Wait
 import java.lang.Thread.sleep
 import java.util.*
@@ -217,25 +218,38 @@ fun taskManagerTest(taskManager: TaskManager) = funSpec {
 }
 
 
-
-
 class TaskManagerErrorHandling: FunSpec({
-    class UnhadledError : Exception()
+    class UnhandledError : Exception()
 
     val errorHandler = mockk<TaskErrorHandler>()
     every { errorHandler.invoke(any(), any()) } returns Unit
 
+    val serialisationErrorHandler = mockk<TaskErrorHandler>()
+    every { serialisationErrorHandler.invoke(any(), any()) } returns Unit
+
     val tm = TaskManager(
         LocalBroker(),
         taskErrorHandlers = listOf(
-            UnhadledError::class.java to errorHandler
+            SerializationException::class.java to serialisationErrorHandler,
+            UnhandledError::class.java to errorHandler
         )
     )
 
     val testTask1 =
         Task.create("failing-task-${randomSuffix()}",) { ctx, input: TaskTrackExecutionWithContextCountInput ->
-            throw UnhadledError()
+            throw UnhandledError()
         }
+
+    @Serializable
+    data class TestTask2Input(
+        val field1: String
+    )
+
+    val testTask2 = Task.create("failing-task-${randomSuffix()}") { ctx, input: TestTask2Input -> }
+
+    beforeTest {
+        clearMocks(errorHandler, serialisationErrorHandler)
+    }
 
     test("test error") {
         tm.startWorkers(testTask1)
@@ -243,9 +257,20 @@ class TaskManagerErrorHandling: FunSpec({
         TaskTrackExecutionWithContextCountInput.new().let {
             testTask1.callLater(it)
             eventually(1000) {
+                verify( exactly = 0 ) { serialisationErrorHandler.invoke(any(), any()) }
                 verify(exactly = 1) { errorHandler.invoke(any(), any()) }
             }
+        }
+    }
 
+    test("test serialisation error") {
+        tm.enqueueTaskCall(testTask2, "uadsfoianf", CallParams())
+
+        tm.startWorkers(testTask2)
+
+        eventually(1000) {
+            verify( exactly = 0 ) { errorHandler.invoke(any(), any()) }
+            verify( exactly = 1 ) { serialisationErrorHandler.invoke(any(), any()) }
         }
     }
 })
